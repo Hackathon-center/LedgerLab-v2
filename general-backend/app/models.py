23f9d38281d60a245ee5f1f206a62f83d88847aa
mehrnaz-MNH <mnh.mehrnaz@gmail.com>
2025-02-18 18:47:10 +1000

    models updated

--- general-backend/app/models.py
+++ general-backend/app/models.py
@@ -1,19 +1,26 @@
 from app import db
 from datetime import datetime
 
+
+class User(db.Model):
+    wallet_id = db.Column(db.String, primary_key=True)
+
+
 class Meme(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     picture = db.Column(db.String, nullable=False)
     title = db.Column(db.String, nullable=False)
     trend_score = db.Column(db.Float, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    created_at = db.Column(db.DateTime, default=datetime.timestamp)
 
-class Token(db.Model):
+class Tokens(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     meme_id = db.Column(db.Integer, db.ForeignKey("meme.id"), nullable=False)
+    wallet_id = db.Column(db.String, db.ForeignKey("user.wallet_id"), nullable=False)
     token_name = db.Column(db.String, nullable=False)
     supply = db.Column(db.Integer)
     minted_at = db.Column(db.DateTime, default=datetime.timestamp)
     status = db.Column(db.String, nullable=False)
 
     meme = db.relationship("Meme", backref="tokens")
+    user = db.relationship("User", backref="tokens")
