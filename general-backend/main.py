3c3cfa2c602058bbbcf950db24223b0ac65caadf
Wilson <wilsonfzheng@gmail.com>
2025-02-28 22:42:21 -0500

    add home dir

--- general-backend/main.py
+++ general-backend/main.py
@@ -1,6 +1,11 @@
 from app import create_app
+from flask import jsonify
 
 app = create_app()
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    return "Welcome to the API"
 
 if __name__ == "__main__":
     app.run(debug=True)
